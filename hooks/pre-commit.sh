8c1a972aee84fa06298e8d339a85f01bf58403e0
vitalets <vitalets@yandex-team.ru>
2016-03-31 21:18:16 +0300

    check only staged files in pre-commit

---
+++ hooks/pre-commit.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# check only staged files in pre-commit hook
+
+modified_files=$( (git diff --name-only --diff-filter=ACM --ignore-submodules --cached) | grep -i '\.js$')
+
+if [[ -n "$modified_files" ]]
+then
+    eslint ${modified_files}
+    jscs ${modified_files}
+else
+    echo "There is no staged js files."
+fi
