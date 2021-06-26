e3489adde051720092094a9a0d5403b1f900796a
root <root@Marlone.localdomain>
2021-06-26 05:47:33 +0000

    Added CSS types

--- types/test.ts
+++ types/test.ts
@@ -2,37 +2,41 @@
  * Type definition tests.
  */
 
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 import EStyleSheet from '..';
 
 const eStyles = EStyleSheet.create({
-    $var: 10,
-    button1: {
-        width: () => '100%',
-        '@media (min-width: 350)': {
-            width: '$var'
-        }
+  $var: 10,
+  button1: {
+    width: () => '100%',
+    '@media (min-width: 350)': {
+      width: '$var',
     },
-    '@media ios': {
-        button2: {
-            width: '100%',
-        }
-    }
+  },
+  button2: {
+    width: () => '100%',
+  },
+
+  '@media ios': {
+    button2: {
+      width: '100%',
+    },
+  },
 });
 
 const styles = StyleSheet.create({
-    button1: {
-        ...EStyleSheet.absoluteFillObject,
-        color: 'red'
-    },
-    button2: {
-        color: 'blue',
-        borderBottomWidth: EStyleSheet.hairlineWidth,
-    }
+  button1: {
+    ...EStyleSheet.absoluteFillObject,
+    color: 'red',
+  },
+  button2: {
+    color: 'blue',
+    borderBottomWidth: EStyleSheet.hairlineWidth,
+  },
 });
 
 EStyleSheet.build();
-EStyleSheet.build({$var: 'foo'});
+EStyleSheet.build({ $var: 'foo' });
 EStyleSheet.value('100%');
 EStyleSheet.value('100%', 'width');
 EStyleSheet.subscribe('build', () => {});
